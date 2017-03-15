bf6535a3e86c4849df12b9eb21d11e31eccfc412
Matt Sachs <mattsachs@Matts-MacBook-Air.local>
2017-03-15 10:38:53 -0700

    add SMA again

--- test.py
+++ test.py
@@ -20,27 +20,12 @@ sectionColor2 = "\033[96m"
 mainColor = "\033[92m"
 
 #Create ROI in Standard Space 
-#1) Derrfuss et al. 
-Inferior frontal junction (6/8/44), left: -40,4,32 (Talairach) --> -41,4,33
-ACC/pre-SMA (32/6), right: 2,14,42 (Talairach) --> 2,12,46
-ACC/SFG (32/9), left: -2, 36, 26 --> -2, 37, 28
-
-#2) Laird et al 2005
-anterior cingulate --> (2,16,41) --> 2,14,45
-left IFJ --> -44,6,34 --> -45,6,36
-rostal cingulate zone posterior --> 2,16,41 --> 2,14,45
-rostral cingulate zone anterior (-3,37,25) --> -3,38,27
-
 #1) IFG
 fslmaths /usr/local/fsl/data/standard/MNI152_T1_2mm_brain.nii.gz -mul 0 -add 1 -roi 46 1 64 1 65  1 0 1 /Volumes/MusicProject/Longitudinal_study/Functional/sma_cluster_point_new -odt float
 fslmaths /Volumes/MusicProject/Longitudinal_study/Functional/sma_cluster_point_new -kernel sphere 8 -fmean /Volumes/MusicProject/Longitudinal_study/Functional/sma_cluster_sphere_new -odt float
 fslmaths /Volumes/MusicProject/Longitudinal_study/Functional/sma_cluster_sphere_new -bin /Volumes/MusicProject/Longitudinal_study/Functional/sma_cluster_sphere_new_bin
 
-#2) pre-SMA/SMA
-fslmaths /usr/local/fsl/data/standard/MNI152_T1_2mm_brain.nii.gz -mul 0 -add 1 -roi 46 1 64 1 65  1 0 1 /Volumes/MusicProject/Longitudinal_study/Functional/sma_cluster_point_new -odt float
-fslmaths /Volumes/MusicProject/Longitudinal_study/Functional/sma_cluster_point_new -kernel sphere 8 -fmean /Volumes/MusicProject/Longitudinal_study/Functional/sma_cluster_sphere_new -odt float
-fslmaths /Volumes/MusicProject/Longitudinal_study/Functional/sma_cluster_sphere_new -bin /Volumes/MusicProject/Longitudinal_study/Functional/sma_cluster_sphere_new_bin
-
+#2) 
 
 
 #set locations
